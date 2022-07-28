d9987269a38173b21a75c5bc96fc7716aa3aa1e7
JBerat <berat.99@hotmail.com>
2022-07-28 20:00:26 +0300

    Added time to answer questions

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const ui = new UI();
 
 ui.btn_start.addEventListener("click", function() {
     ui.quiz_box.classList.add("active")
+    startTimer(10);
     ui.soruGoster(quiz.soruGetir());
     ui.soruSayisiniGoster(quiz.soruIndex + 1, quiz.sorular.length)
     ui.btn_next.classList.remove("show")
@@ -12,16 +13,17 @@ ui.btn_start.addEventListener("click", function() {
 
 ui.btn_next.addEventListener("click", function() {
     if(quiz.sorular.length != quiz.soruIndex + 1) {
-        ui.quiz_box.classList.add("active")
         quiz.soruIndex += 1;
-        ui.soruGoster(quiz.soruGetir())
-        ui.soruSayisiniGoster(quiz.soruIndex + 1, quiz.sorular.length)
-        ui.btn_next.classList.remove("show")
+        clearInterval(counter);
+        startTimer(10);
+        ui.soruGoster(quiz.soruGetir());
+        ui.soruSayisiniGoster(quiz.soruIndex + 1, quiz.sorular.length);
+        ui.btn_next.classList.remove("show");
     } else {
-        console.log("quiz is finished")
-        ui.quiz_box.classList.remove("active")
-        ui.score_box.classList.add("active")
-        ui.skoruGoster(quiz.sorular.length, quiz.dogruCevapSayisi)
+        clearInterval(counter);
+        ui.quiz_box.classList.remove("active");
+        ui.score_box.classList.add("active");
+        ui.skoruGoster(quiz.sorular.length, quiz.dogruCevapSayisi);
     }
 })
 
@@ -38,6 +40,7 @@ ui.btn_replay.addEventListener("click", function() {
 })
 
 function optionSelected(option) {
+    clearInterval(counter)
     let cevap = option.querySelector("span b").textContent;
     let soru = quiz.soruGetir();
 
@@ -55,4 +58,33 @@ function optionSelected(option) {
     }
 
     ui.btn_next.classList.add("show")
+}
+
+let counter;
+function startTimer(time) {
+    counter = setInterval(timer, 1000);
+
+    function timer() {
+        ui.time_second.textContent = time;
+        time--;
+
+        if(time < 0) {
+            clearInterval(counter);
+
+            ui.time_text.textContent = "Süre Bitti";
+
+            let cevap = quiz.soruGetir().dogruCevap;
+
+            for(let option of ui.option_list.children) {
+                if(option.querySelector("span b").textContent == cevap) {
+                    option.classList.add("correct")
+                    option.insertAdjacentHTML("beforeend", ui.correctIcon)
+                }
+
+                option.classList.add("disabled")
+            }
+
+            ui.btn_next.classList.add("show")
+        }
+    }
 }
--- soru.js
+++ soru.js
@@ -10,7 +10,7 @@ Soru.prototype.cevabiKontrolEt = function(cevap) {
 
 let sorular = [
     new Soru("1-Hangisi js paket yönetim uygulamasıdır?", {a: "Node.js", b: "TypeScript", c: "Npm", d: "Nuget"}, "c"),
-    new Soru("2-Hangisi javascript paket yönetim uygulamasıdır?", {a: "Node.js", b: "TypeScript", c: "Npm"}, "c"),
-    new Soru("3-Hangisi javascript paket yönetim uygulamasıdır?", {a: "Node.js", b: "TypeScript", c: "Npm"}, "c"),
-    new Soru("4-Hangisi javascript paket yönetim uygulamasıdır?", {a: "Node.js", b: "TypeScript", c: "Npm"}, "c")
+    new Soru("2-Hangisi frontend kapsamında değerlendirilmez?", {a: "css", b: "html", c: "javascript", d: "sql"}, "d"),
+    new Soru("3-Hangisi backend kapsamında değerlendirilir?", {a: "Node.js", b: "TypeScript", c: "angular", d: "react"}, "a"),
+    new Soru("4-Hangisi javascript programlama dilini kullanmaz?", {a: "react", b: "angular", c: "vue.js", d: "asp.net"}, "d")
 ]
