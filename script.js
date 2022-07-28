af07ceb70b36539bd73b112edd27ef5ee3104f6e
JBerat <berat.99@hotmail.com>
2022-07-28 13:19:18 +0300

    questions shown

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ Soru.prototype.cevabiKontrolEt = function(cevap) {
 }
 
 let sorular = [
-    new Soru("1-Hangisi js paket yönetim uygulamasıdır?", {a: "Node.js", b: "TypeScript", c: "Npm"}, "c"),
+    new Soru("1-Hangisi js paket yönetim uygulamasıdır?", {a: "Node.js", b: "TypeScript", c: "Npm", d: "Nuget"}, "c"),
     new Soru("2-Hangisi javascript paket yönetim uygulamasıdır?", {a: "Node.js", b: "TypeScript", c: "Npm"}, "c"),
     new Soru("3-Hangisi javascript paket yönetim uygulamasıdır?", {a: "Node.js", b: "TypeScript", c: "Npm"}, "c"),
     new Soru("4-Hangisi javascript paket yönetim uygulamasıdır?", {a: "Node.js", b: "TypeScript", c: "Npm"}, "c")
@@ -31,12 +31,35 @@ const quiz = new Quiz(sorular);
 
 
 document.querySelector(".btn_start").addEventListener("click", function() {
-    if(quiz.sorular.length != quiz.soruIndex) {
+    document.querySelector(".quiz_box").classList.add("active")
+    soruGoster(quiz.soruGetir());
+    
+})
+
+document.querySelector(".next_btn").addEventListener("click", function() {
+    if(quiz.sorular.length != quiz.soruIndex + 1) {
         document.querySelector(".quiz_box").classList.add("active")
-        console.log(quiz.soruGetir());
         quiz.soruIndex += 1;
+        soruGoster(quiz.soruGetir())   
     } else {
         console.log("quiz is finished")
     }
-    
 })
+
+function soruGoster(soru) {
+    let question = `<span>${soru.soruMetni}</span>`
+    let options = "";
+
+    for(let cevap in soru.cevapSecenekleri) {
+        options += 
+        `
+            <div class="option">
+                <span><b>${cevap}</b>: ${soru.cevapSecenekleri[cevap]}</span>
+            </div>
+
+        `;
+    }
+
+    document.querySelector(".question_text").innerHTML = question;
+    document.querySelector(".option_list").innerHTML = options;
+}
