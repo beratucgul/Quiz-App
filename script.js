8435416a40f64a26bfa57347efa4b8e512c01dee
JBerat <berat.99@hotmail.com>
2022-07-28 13:40:40 +0300

    answer options check complete

--- script.js
+++ script.js
@@ -33,6 +33,7 @@ const quiz = new Quiz(sorular);
 document.querySelector(".btn_start").addEventListener("click", function() {
     document.querySelector(".quiz_box").classList.add("active")
     soruGoster(quiz.soruGetir());
+    document.querySelector(".next_btn").classList.remove("show")
     
 })
 
@@ -41,10 +42,15 @@ document.querySelector(".next_btn").addEventListener("click", function() {
         document.querySelector(".quiz_box").classList.add("active")
         quiz.soruIndex += 1;
         soruGoster(quiz.soruGetir())   
+        document.querySelector(".next_btn").classList.remove("show")
     } else {
         console.log("quiz is finished")
     }
 })
+
+const option_list = document.querySelector(".option_list");
+const correctIcon = '<div class="icon"><i class="fas fa-check"></i></div>';
+const incorrectIcon = '<div class="icon"><i class="fas fa-times"></i></div>';
 
 function soruGoster(soru) {
     let question = `<span>${soru.soruMetni}</span>`
@@ -59,7 +65,32 @@ function soruGoster(soru) {
 
         `;
     }
-
+    
     document.querySelector(".question_text").innerHTML = question;
-    document.querySelector(".option_list").innerHTML = options;
+    option_list.innerHTML = options;
+
+    const option = option_list.querySelectorAll(".option")
+
+    for(let opt of option) {
+        opt.setAttribute("onclick", "optionSelected(this)")
+    }
+}
+
+function optionSelected(option) {
+    let cevap = option.querySelector("span b").textContent;
+    let soru = quiz.soruGetir();
+
+    if(soru.cevabiKontrolEt(cevap)) {
+        option.classList.add("correct")
+        option.insertAdjacentHTML("beforeend", correctIcon)
+    } else {
+        option.classList.add("incorrect")
+        option.insertAdjacentHTML("beforeend", incorrectIcon)
+    }
+
+    for(let i=0; i < option_list.children.length; i++) {
+        option_list.children[i].classList.add("disabled")
+    }
+
+    document.querySelector(".next_btn").classList.add("show")
 }
